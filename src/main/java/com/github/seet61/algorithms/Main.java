c8fda206de745c227e3fd1d8afcd347557ef6a6d
dmitry.a.arefyev <dmitry.a.arefyev@tele2.ru>
2023-11-02 16:09:45 +0300

    double chars

--- src/main/java/com/github/seet61/algorithms/Main.java
+++ src/main/java/com/github/seet61/algorithms/Main.java
@@ -3,5 +3,27 @@ package com.github.seet61.algorithms;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        maxConsecutiveElements("");
+        maxConsecutiveElements("abcd");
+        maxConsecutiveElements("abbcd");
+        maxConsecutiveElements("abbcccd");
+        maxConsecutiveElements("abbcccdddd");
+    }
+
+    private static void maxConsecutiveElements(String inputStr) {
+        System.out.print(inputStr + ": ");
+        char[] inputStrSplitted = inputStr.toCharArray();
+        int result = 0;
+        int curLetterIdx = 0;
+        while (curLetterIdx < inputStr.length()) {
+            int nextLetterIdx = curLetterIdx;
+            while (nextLetterIdx < inputStr.length() && inputStrSplitted[nextLetterIdx] == inputStrSplitted[curLetterIdx]) {
+                nextLetterIdx += 1;
+            }
+            result = Math.max(nextLetterIdx - curLetterIdx, result);
+            curLetterIdx = nextLetterIdx;
+        }
+        System.out.println(result);
+        //return result;
     }
 }
